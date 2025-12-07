46554fd05db0816e036512752a575278fd47e8f4
JHON ALEXANDER AGUIRRE IZQUIERDO <jhoaguizq@alu.edu.gva.es>
2025-12-07 12:45:12 +0100

    Precios cambiados

--- EjercicioTelegrama/Form1.cs
+++ EjercicioTelegrama/Form1.cs
@@ -53,11 +53,11 @@ namespace EjercicioTelegrama
             {
                 if (numPalabras <= 10)
                 {
-                    coste = 2.5;
+                    coste = 3;
                 }
                 else
                 {
-                    coste = 2.5 + 0.5 * (numPalabras - 10);
+                    coste = 3 + 0.5 * (numPalabras - 10);
                 }
             }
             else
@@ -67,11 +67,11 @@ namespace EjercicioTelegrama
                 {
                     if (numPalabras <= 10)
                     {
-                        coste = 5;
+                        coste = 6;
                     }
                     else
                     {
-                        coste = 5 + 0.75 * (numPalabras - 10);
+                        coste = 6 + 0.75 * (numPalabras - 10);
                     }
                 }
                 else
